907d65b5e32a902f7a31444acd48ec9936753077
DID0101 <didarpurliyew750@gmail.com>
2023-05-12 15:05:22 +0300

    first commit

---
+++ Client/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFullStackCrud.Client.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+        List<SuperHero>  Heroes { get; set; }
+        List<Comic> Comics { get;set; }
+        Task GetComic();
+        Task GetSuperHeroes();
+        Task <SuperHero> GetSingleHero(int id);
+    }
+}
